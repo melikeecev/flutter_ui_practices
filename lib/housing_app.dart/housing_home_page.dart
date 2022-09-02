91fed8d175152e0d38655cf09c005d2c4df4245a
melikeecev <melike_cevik1@outlook.com>
2022-09-02 23:47:28 +0300

    housing ui first commit

---
+++ lib/housing_app.dart/housing_home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HousingHomePage extends StatefulWidget {
+  const HousingHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HousingHomePage> createState() => _HousingHomePageState();
+}
+
+class _HousingHomePageState extends State<HousingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(),
+      ),
+    );
+  }
+}
