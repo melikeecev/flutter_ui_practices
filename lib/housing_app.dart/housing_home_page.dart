cf6845e46fe59da900bb5659b3be2f26a629efe0
melikeecev <melike_cevik1@outlook.com>
2022-09-03 23:41:13 +0300

    housing home page

--- lib/housing_app.dart/housing_home_page.dart
+++ lib/housing_app.dart/housing_home_page.dart
@@ -8,36 +8,181 @@ class HousingHomePage extends StatefulWidget {
 }
 
 class _HousingHomePageState extends State<HousingHomePage> {
+  PageController? _pageController;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      viewportFraction: 0.5, 
+      initialPage: 0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                CircleAvatar(),
-                Expanded(child: Column(
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const CircleAvatar(),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Location'),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            children: const [
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.indigo,
+                                size: 16,
+                              ),
+                              Text('Semerang City'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 48,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.apps,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  left: 16,
+                  bottom: 16,
+                ),
+                child: PageView(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(right: 16),
+                      child: Card(
+                        child: Center(),
+                      ),
+                    ),
+                    Card(
+                      child: Center(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: 78,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(48),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text('Location'),
-                    Row(
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Icon(
-                          Icons.location_on,
-                          color: Colors.indigo,
+                          Icons.compass_calibration,
+                          color: Colors.white,
                         ),
-                        Text('Semerang City'),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          height: 2,
+                          width: 24,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.map_outlined,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          height: 2,
+                          width: 24,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          height: 2,
+                          width: 24,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.chat_outlined,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          height: 2,
+                          width: 24,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                        ),
                       ],
                     ),
                   ],
-                ),),
-              ],
-            ),
-            Expanded(child: Placeholder(),),
-            Container(
-              height: 72,
-              decoration: BoxDecoration(
-                color: Colors.black,
+                ),
               ),
             ),
           ],
@@ -46,4 +191,3 @@ class _HousingHomePageState extends State<HousingHomePage> {
     );
   }
 }
-//6.39
